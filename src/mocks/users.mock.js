d4e2bd59df6b3159e5852cfd5485e966a006f876
unknown <ricardo.gp2007@gmail.com>
2025-03-29 15:50:15 -0400

    preEntrega1

--- src/mocks/users.mock.js
+++ src/mocks/users.mock.js
@@ -4,22 +4,25 @@ import { createHash } from '../utils/index.js';
 const roles = ['user', 'admin'];
 
 export const generateUser = async () => {
-  const hashedPassword = await createHash('coder123');
-  return {
-    _id: faker.database.mongodbObjectId(),
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    email: faker.internet.email(),
-    password: hashedPassword,
-    role: faker.helpers.arrayElement(roles),
-    pets: []
-  };
+    const hashedPassword = await createHash('coder123');
+    
+    return {
+        _id: faker.database.mongodbObjectId(),
+        first_name: faker.person.firstName(), 
+        last_name: faker.person.lastName(), 
+        email: faker.internet.email(),
+        password: hashedPassword,
+        role: faker.helpers.arrayElement(roles),
+        pets: []
+    };
 };
 
 export const generateManyUsers = async (count) => {
-  const users = [];
-  for (let i = 0; i < count; i++) {
-    users.push(await generateUser());
-  }
-  return users;
+    const users = [];
+    
+    for (let i = 0; i < count; i++) {
+        users.push(await generateUser());
+    }
+    
+    return users;
 };
